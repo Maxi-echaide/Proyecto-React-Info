3ac978fb95c71aa84d704aa8514eb34e73cf5bcc
Maxi-echaide <maxi.echaide@gmail.com>
2022-12-21 04:06:38 -0300

    ultimo2

--- my-app-react/src/Components/Header y footer/Footer.jsx
+++ my-app-react/src/Components/Header y footer/Footer.jsx
@@ -1,28 +1,9 @@
-/*import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-*/
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Paper from '@mui/material/Paper';
-import Fab from '@mui/material/Fab';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import Avatar from '@mui/material/Avatar';
-import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
+
+
 
 
 
@@ -32,40 +13,22 @@ import MoreIcon from '@mui/icons-material/MoreVert';
     return (
         <Box  position= 'fixed' sx={{bottom: 0,
         width: '100%'}} >
-        <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <AppBar position="sticky"  sx={{flexGrow: 0, width: '408px', marginBottom:"0px",  bottom:0, textAlign:"center", justifyContent:"center" }}>
           <Toolbar variant="regular">
            
-            <Typography variant="h6" color="inherit" component="div" pl={58} sx={{}}  >
+            <Typography variant="h6" color="inherit" component="div" pl={14} sx={{textAlign:"center"}}  >
                    Copyright 2022
             </Typography>
           </Toolbar>
         </AppBar>
       </Box>
       );
+        
+    
+              
+    
+        
 }
 
-
-
-/* const Footer = () => {
-    return (
-        <React.Fragment>
-        
-      
-        <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
-          <Toolbar variant="regular">
-            
-            
-            
-            <Typography variant="h6" color="inherit" component="div"  sx={{ marginLeft:"400px"}}  >
-                   Copyright 2022
-            </Typography>
-           
-          </Toolbar>
-        </AppBar>
-      </React.Fragment>
-
-        )
-    }
-*/
 
 export default Footer
--- my-app-react/src/Components/Header y footer/header.jsx
+++ my-app-react/src/Components/Header y footer/header.jsx
@@ -1,23 +1,18 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
 
-  
-  
-/*
+
 export const Header = () => {
     return (
-        <Box sx={{ bottom: 0,
-            width: '100%' }}>
-        <AppBar position="relative"  sx={{ top: 'auto', bottom: 0 }}>
+        <Box sx={{ flexGrow: 0 }}>
+        <AppBar position="relative"  sx={{ maxWidth: "408px" }}>
           <Toolbar variant="dense">
            
-            <Typography variant="h6" color="inherit" component="div" pl={12}  sx={{ }}>
+            <Typography variant="h6" color="inherit" component="div"   sx={{ }}>
               Buscador de noticias
             </Typography>
           </Toolbar>
@@ -25,19 +20,3 @@ export const Header = () => {
       </Box>
       );
 }
-*/
-
-export const Header = () => {
-    return ( <Box position= 'absolute' mt={-10} ml={-45} sx={{width: '100%'  }}>
-        <AppBar position="relative" sx={{ top: 'auto', width: '100%' }}>
-          <Toolbar variant="regular">
-            
-            <Typography variant="h6" component="div" sx={{  }}>
-              Buscador de noticias
-            </Typography>
-            
-          </Toolbar>
-        </AppBar>
-      </Box>)
-}
-export default Header
